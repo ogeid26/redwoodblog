66d65713391f6b50d8a74eb862225e98b219f2d6
diego <dmalvaradog26@gmail.com>
2023-04-04 20:27:02 -0300

    created contact page

--- web/src/pages/ContactPage/ContactPage.tsx
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -1,9 +1,11 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
-import { Form, Submit, SubmitHandler, TextField } from '@redwoodjs/forms'
+import { Form, Submit, SubmitHandler, TextAreaField, TextField } from '@redwoodjs/forms'
 
 interface FormValues {
-  input: string
+  name: string
+  email: string
+  message: string
 }
 
 const ContactPage = () => {
@@ -14,8 +16,14 @@ const ContactPage = () => {
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
+
       <Form onSubmit={onSubmit}>
-        <TextField name="input" />
+        <label htmlFor="name">Name</label>
+        <TextField name="name" />
+        <label htmlFor="email">Email</label>
+        <TextField name="email" />
+        <label htmlFor="message">Message</label>
+        <TextAreaField name="message" />
         <Submit>Save</Submit>
       </Form>
           </>
