e6ed731ca6a006332c08e9e73b1215c024cfebde
diego <dmalvaradog26@gmail.com>
2023-04-04 20:42:36 -0300

    created the contact page with forms

--- web/src/pages/ContactPage/ContactPage.tsx
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -1,6 +1,14 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
-import { Form, Submit, SubmitHandler, TextAreaField, TextField } from '@redwoodjs/forms'
+import {
+  Form,
+  Submit,
+  SubmitHandler,
+  TextAreaField,
+  TextField,
+  FieldError,
+  Label
+} from '@redwoodjs/forms'
 
 interface FormValues {
   name: string
@@ -17,18 +25,43 @@ const ContactPage = () => {
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <Form onSubmit={onSubmit}>
-        <label htmlFor="name">Name</label>
-        <TextField name="name" />
-        <label htmlFor="email">Email</label>
-        <TextField name="email" />
-        <label htmlFor="message">Message</label>
-        <TextAreaField name="message" />
+      <Form onSubmit={onSubmit} config={{mode: 'onBlur'}}>
+
+        <Label name="name" errorClassName='error'>Name</Label>
+        <TextField
+          name="name"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="name" className="error" />
+
+        <Label name="email" errorClassName='error'>Email</Label>
+        <TextField
+          name="email"
+          validation={
+            {required: true,
+              pattern: {
+                value: /^[^@]+@[^.]+\..+$/,
+                message: 'Please enter a valid email adress',
+              },
+           }}
+          errorClassName="error"
+        />
+        <FieldError name="email" className="error" />
+
+        <Label name="message" errorClassName='error'>Message</Label>
+        <TextAreaField
+          name="message"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="message" className="error" />
+
+
         <Submit>Save</Submit>
       </Form>
-          </>
+    </>
   )
 }
 
 export default ContactPage
-
